675b47de8c666c6709efc1a0646d78abc959be90
grohacks <nishantrao.2005@gmail.com>
2025-08-21 11:23:05 +0530

    Added subtraction functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,90 @@
-import React, { Component } from 'react';
-import './App.css';
-import { callApi,BASEURL } from './api';
+import React, { Component } from "react";
+import "./App.css";
+import { callApi, BASEURL } from "./api";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={A:0,B:0,RES:0};
+    this.state = { A: 0, B: 0, RES: 0 };
     this.getResponse = this.getResponse.bind(this);
   }
 
-  add(){
-    callApi("GET", BASEURL + `add/${this.state.A}/${this.state.B}`, "", this.getResponse);
+  add() {
+    callApi(
+      "GET",
+      BASEURL + "add/" + this.state.A + "/" + this.state.B,
+      "",
+      this.getResponse
+    );
   }
 
-  getResponse(res){
-    this.setState({RES: res});
+  sub() {
+    callApi(
+      "GET",
+      BASEURL + "sub/" + this.state.A + "/" + this.state.B,
+      "",
+      this.getResponse
+    );
   }
 
-  loadInputChange(event){
-      this.setState({[event.target.name] : event.target.value});
+  getResponse(res) {
+    this.setState({ RES: res });
+  }
+
+  loadInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    const {A, B, RES} = this.state;
+    const { A, B, RES } = this.state;
     return (
       <>
         <header>
-          <div className='logo'>Simple Calculator</div>
+          <div className="logo">Simple Calculator</div>
         </header>
         <section>
           <table>
             <tr>
               <td>Enter the value of A</td>
-              <td><input type='text' id='T1' name='A' value={A} onChange={(event)=>this.loadInputChange(event)} /></td>
+              <td>
+                <input
+                  type="text"
+                  id="T1"
+                  name="A"
+                  value={A}
+                  onChange={(event) => this.loadInputChange(event)}
+                />
+              </td>
             </tr>
             <tr>
               <td>Enter the value of B</td>
-              <td><input type='text' id='T2' name='B' value={B} onChange={(event)=>this.loadInputChange(event)} /></td>
+              <td>
+                <input
+                  type="text"
+                  id="T2"
+                  name="B"
+                  value={B}
+                  onChange={(event) => this.loadInputChange(event)}
+                />
+              </td>
             </tr>
             <tr>
               <td>Result</td>
-              <td><label id='L1'>{RES}</label></td>
+              <td>
+                <label id="L1">{RES}</label>
+              </td>
             </tr>
             <tr>
               <td></td>
-              <td><button onClick={()=>this.add()}>ADD</button></td>
+              <td>
+                <button onClick={() => this.add()}>ADD</button>
+                <button
+                  onClick={() => this.sub()}
+                  style={{ marginLeft: "10px" }}
+                >
+                  SUBTRACT
+                </button>
+              </td>
             </tr>
           </table>
         </section>
